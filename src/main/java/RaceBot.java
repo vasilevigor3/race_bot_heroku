61e110b96efd9571d6c8c461273009f6be5fe6b6
igor <vasilevigor3@gmail.com>
2019-12-26 20:12:00 +0400

    1

---
+++ src/main/java/RaceBot.java
@@ -0,0 +1,28 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class RaceBot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "race_test_bot";
+    }
+
+    public String getBotToken() {
+        return "986919353:AAG08q18WnpsPtEWLRljzCDlNIkBFeAegOw";
+    }
+}
