6509de5fbf7f1babe790373acc684096c337112d
igor <vasilevigor3@gmail.com>
2019-12-27 02:07:59 +0400

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,14 @@
+import lombok.SneakyThrows;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
 
+    @SneakyThrows
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
-        try {
             botsApi.registerBot(new RaceBot());
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
-        }
     }
 
 }
