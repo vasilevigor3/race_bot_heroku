2ad69ee09538a2325b3ff6b742a56f716e26e069
igor <vasilevigor3@gmail.com>
2019-12-28 02:22:31 +0400

    pomUp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,26 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
+    private static final String PORT = System.getenv("PORT");
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
         try {
             botsApi.registerBot(new RaceBot());
         } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+        try (ServerSocket serverSocket = new ServerSocket(Integer.valueOf(PORT))) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
